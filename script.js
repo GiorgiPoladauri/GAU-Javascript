8bf6f8131a61a63b411eb0ea8910eca807323b46
GiorgiPoladauri <giorgi.poladauri15@yahoo.com>
2025-01-17 00:25:28 +0400

    Update Number INFINITE...

--- script.js
+++ script.js
@@ -2,39 +2,51 @@ let currentIndex = 0;
 const slides = document.querySelectorAll('.subdiv4 img');
 
 function changeSlide() {
-    
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
-    }
-    
-    slides[currentIndex].style.display = 'block';
+    slides.forEach(slide => {
+        slide.classList.add('black-out');
+    });
 
-    currentIndex = currentIndex + 1;
-    if (currentIndex >= slides.length) {
-        currentIndex = 0;
-    }
+    setTimeout(() => {
+        slides.forEach(slide => {
+            slide.classList.remove('active', 'black-out');
+        });
+
+        slides[currentIndex].classList.add('active');
+        
+        currentIndex = (currentIndex + 1) % slides.length;
+    }, 2000);
 }
 
-setInterval(changeSlide, 3000);
+setInterval(changeSlide, 6000);
 
-changeSlide();
+changeSlide(); 
 
-// Function to fetch and apply the selected language
-function changeLanguage(language) {
-    fetch('languages.json')  // Load the JSON file
-        .then(response => response.json())  // Parse the JSON file
-        .then(data => {
-            // Update the HTML elements based on the selected language
+
+///
+
+let currentLanguage = 'en';
+
+const changeLanguage = (language) => {
+    fetch('languages.json')
+        .then((response) => response.json())
+        .then((data) => {
             document.getElementById('text1').textContent = data[language].text1;
             document.getElementById('text2').textContent = data[language].text2;
             document.getElementById('text3').textContent = data[language].text3;
             document.getElementById('text4').textContent = data[language].text4;
             document.getElementById('text5').textContent = data[language].text5;
         })
-        .catch(error => console.error('Error loading language file:', error));
-}
-
-// Initial load - default language
-window.onload = function() {
-    changeLanguage('en');  // Set default language to English
+        .catch((error) => console.error('Error loading language file:', error));
 };
+
+const toggleLanguage = () => {
+    currentLanguage = currentLanguage === 'en' ? 'ge' : 'en';
+    changeLanguage(currentLanguage);
+};
+
+window.onload = () => {
+    changeLanguage(currentLanguage);
+};
+
+///
+
